ba68e1aff8199cc890a575190fa2fb5e5200115c
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-02-10 20:49:15 +0100

    Gjorde om systemet til å bruke en cache

--- project/src/main/java/spill/storage/Storage.java
+++ project/src/main/java/spill/storage/Storage.java
@@ -1,6 +1,7 @@
 package spill.storage;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,16 +9,60 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Storage implements StorageInteface {
     // Mye av koden her er hentet og modifisert fra eksempeloppgaven (TodoList)
     // Endringene kommer av at jeg har en annen måte å lagre data på. Dataene her er på formen "PROPERTY=value", der både property og value er vilkårlige strenger (Feks JSON)
     // Lagringssystemet blir da mer åpent og man er ikke låst til et strengt bruksområde
+    //Har også laget et cachingsystem så man slipper å lese fila hver gang
 
-    public final static String SAVE_EXTENSION = "gamesave";
-    public final static String PROPERY_VALUE_DIVIDER = "=";
+    private static final String SAVE_EXTENSION = "gamesave";
+    private static final String PROPERY_VALUE_DIVIDER = "=";
+
+    private int saveId;
+
+    private HashMap<String,String> cache;
+
+    public Storage(int saveId){
+        this.saveId = saveId;
+        cache = loadCache();
+    }
+
+    private HashMap<String,String> loadCache(){
+        HashMap<String,String> cache = new HashMap<>();
+        try (InputStream is = new FileInputStream(getSavePath(saveId).toFile())) {
+            try (Scanner scanner = new Scanner(is)) {
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    Pattern dataPattern = Pattern.compile("^(?<property>(?:[A-Z_])+)=(?<value>.+)$");
+                    Matcher dataMatch = dataPattern.matcher(line);
+                    dataMatch.find();
+                    cache.put(dataMatch.group("property"), dataMatch.group("value"));
+                }
+            }
+        } catch (FileNotFoundException e) {
+            
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        return cache;
+    }
+
+    private void saveCache() throws IOException{
+        var savePath = getSavePath(saveId);
+        ensureGameUserFolder();
+        try (OutputStream os = new FileOutputStream(savePath.toFile())) {
+            try (PrintWriter writer = new PrintWriter(os)) {
+                for (String property : cache.keySet()) {
+                    writer.println(property + PROPERY_VALUE_DIVIDER + cache.get(property));
+                }
+            }
+        }
+    }
 
     private Path getGameUserFolderPath() {
         return Path.of(System.getProperty("user.home"), "tdt4100", "game");
@@ -37,7 +82,7 @@ public class Storage implements StorageInteface {
     }
 
     private String validatePropertyName(String property) throws IllegalArgumentException {
-        Pattern propertyPattern = Pattern.compile("^(?:[A-Z]|\\_)+$");
+        Pattern propertyPattern = Pattern.compile("^(?:[A-Z]|_)+$");
         if (!propertyPattern.matcher(property).find()) {
             throw new IllegalArgumentException("Storage property names can only contain capital letters A-Z and underscores (_) Must contain at least one character");
         }
@@ -52,30 +97,11 @@ public class Storage implements StorageInteface {
         return value;
     }
 
-
     @Override
-    public String readSave(InputStream is, String property) {
-        validatePropertyName(property);
-        try (Scanner scanner = new Scanner(is)) {
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                if (line.startsWith(property + PROPERY_VALUE_DIVIDER)) {
-                    return line.substring(property.length() + 1); //The value of the property
-                }
-            }
-        }
-        return null;
+    public String readSave(String property) throws IOException {
+        return cache.get(property);
     }
 
-    @Override
-    public String readSave(int saveId, String property) throws IOException {
-        var savePath = getSavePath(saveId);
-        try (var input = new FileInputStream(savePath.toFile())) {
-            return readData(input, property);
-        }
-    }
-
-    @Override
     public void writeSave(InputStream is, OutputStream os, String property, String value) {
         validatePropertyName(property);
         validateValue(value);
@@ -93,17 +119,10 @@ public class Storage implements StorageInteface {
     }
 
     @Override
-    public void writeSave(int saveId, String property, String value) throws IOException {
+    public void writeSave(String property, String value) throws IOException {
         validatePropertyName(property);
         validateValue(value);
-        var savePath = getSavePath(saveId);
-        ensureGameUserFolder();
-        try (var is = new FileInputStream(savePath.toFile())) {
-            try (var os = new FileOutputStream(savePath.toFile())) {
-                writeSave(is, os, property, value);
-            }
-        }
-
+        cache.put(property, value);
+        saveCache();
     }
-    
 }
