f6cd6c3f462d25d467df7b28ca76dcc308e6dc7c
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-02-10 20:48:55 +0100

    Fjerna funksjoner for Input-/OutputStream

--- project/src/main/java/spill/storage/StorageInteface.java
+++ project/src/main/java/spill/storage/StorageInteface.java
@@ -1,18 +1,9 @@
 package spill.storage;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 
 public interface StorageInteface {
-    /**
-	 * Read the value of a property from a stream
-	 * @param is The input stream to read from.
-     * @param property The property to return.
-	 * @return The value of the property.
-	 */
-	String readSave(InputStream is, String property);
 	/**
 	 * Read the value of a property in a specific save.
 	 * @param saveId The id of the save
@@ -20,17 +11,7 @@ public interface StorageInteface {
 	 * @return The value of the property.
 	 * @throws IOException if the saveId is invalid
 	 */
-	String readSave(int saveId, String property) throws IOException;
-	
-	/**
-	 * Write the value to the property in the stream. Creates new property if not in input stream
-	 * @param is The stream to read from
-     * @param os The stream to write to
-	 * @param property The property to write to
-     * @param value The value to give the property
-	 * 
-	 */
-	void writeSave(InputStream is, OutputStream os, String property, String value);
+	String readSave(String property) throws IOException;
 	/**
 	 * Write the value to the property in the file with the given id. Creates new property if not in file. Creates new file if none with the given id.
 	 * @param saveId The id of the file to write to
@@ -38,6 +19,6 @@ public interface StorageInteface {
      * @param value The value to give the property
 	 * @throws IOException If a file at the proper location can't be written to
 	 */
-	void writeSave(int saveId, String property, String value) throws IOException;
+	void writeSave(String property, String value) throws IOException;
 
 }
