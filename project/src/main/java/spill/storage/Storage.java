44e2ab1802a565f8c4158ef5ecb2b8cdceb6fcf1
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-02-10 12:00:21 +0100

    Added reading data and validatioing property names

--- project/src/main/java/spill/storage/Storage.java
+++ project/src/main/java/spill/storage/Storage.java
@@ -3,12 +3,55 @@ package spill.storage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+import javax.management.InvalidAttributeValueException;
 
 public class Storage implements StorageInteface {
 
+    public final static String SAVE_EXTENSION = "gamesave";
+    public final static String PROPERY_VALUE_DIVIDER = "=";
+
+    private Path getGameUserFolderPath() {
+        return Path.of(System.getProperty("user.home"), "tdt4100", "game");
+    }
+
+    private boolean ensureGameUserFolder() {
+        try {
+            Files.createDirectories(getGameUserFolderPath());
+            return true;
+        } catch (IOException ioe) {
+            return false;
+        }
+    }
+
+    private Path getSavePath(String name) {
+        return getGameUserFolderPath().resolve(name + "." + SAVE_EXTENSION);
+    }
+
+    private String validatePropertyName(String property) throws IllegalArgumentException {
+        Pattern propertyPattern = Pattern.compile("^(?:[A-Z]|\\_)+$");
+        if (!propertyPattern.matcher(property).find()) {
+            throw new IllegalArgumentException("Storage property names can only contain capital letters A-Z and underscores (_) Must contain at least one character");
+        }
+        return property;
+    }
+
+
     @Override
     public String readData(InputStream is, String property) {
-        // TODO Auto-generated method stub
+        validatePropertyName(property);
+        try (Scanner scanner = new Scanner(is)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.startsWith(property + PROPERY_VALUE_DIVIDER)) {
+                    return line.substring(property.length() + 1);
+                }
+            }
+        }
         return null;
     }
 
