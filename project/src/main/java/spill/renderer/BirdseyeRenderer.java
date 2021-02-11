0550f7719a6231bf9670a11e0ae8473defe2b0c0
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-02-11 11:17:55 +0100

    Rendrer to view game in birdseye view

---
+++ project/src/main/java/spill/renderer/BirdseyeRenderer.java
@@ -0,0 +1,12 @@
+package spill.renderer;
+
+import javafx.scene.canvas.GraphicsContext;
+
+public class BirdseyeRenderer{
+    GraphicsContext gc;
+
+    public BirdseyeRenderer(GraphicsContext gc) {
+        this.gc = gc;
+    }
+
+}
