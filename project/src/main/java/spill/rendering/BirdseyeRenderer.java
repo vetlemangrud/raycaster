de4e544fe4d2a7503ed3f8153fe4f8024e4df0a4
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-02-11 12:01:02 +0100

    Simple rect

--- project/src/main/java/spill/rendering/BirdseyeRenderer.java
+++ project/src/main/java/spill/rendering/BirdseyeRenderer.java
@@ -4,9 +4,12 @@ import javafx.scene.canvas.GraphicsContext;
 
 public class BirdseyeRenderer{
     GraphicsContext gc;
-
     public BirdseyeRenderer(GraphicsContext gc) {
         this.gc = gc;
+    }
+
+    public void render(){
+        gc.fillRect(100, 100, 100, 200);
     }
 
 }
