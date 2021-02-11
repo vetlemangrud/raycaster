12c7ecd7318d4ae5dd36457fd503565730244ca4
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-02-11 11:17:31 +0100

    UI change to create new save

--- project/src/main/java/spill/launcher/LauncherController.java
+++ project/src/main/java/spill/launcher/LauncherController.java
@@ -3,10 +3,13 @@ package spill.launcher;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
+
 import javafx.event.Event;
 
 public class LauncherController {
@@ -18,18 +21,21 @@ public class LauncherController {
     private VBox saveSelectVBox;
 
     @FXML
-    void initialize() throws IOException{
-        for (int i = 0; i < 10; i++){
-            saveSelectVBox.getChildren().add(createStartSaveButton("Save" + i, i));
-        }
+    private TextField saveNameTextField;
+
+    // @FXML
+    // void initialize() throws IOException{
+    //     for (int i = 0; i < 10; i++){
+    //         saveSelectVBox.getChildren().add(createStartSaveButton("Save" + i, i));
+    //     }
         
-    }
+    // }
 
 
     @FXML
     private void createNewSaveButtonAction(Event evt) {
-        //Start new save
-        System.out.println(((Button)evt.getSource()).getId());
+        String saveName = validateSaveName(saveNameTextField.getText());
+        saveSelectVBox.getChildren().add(createStartSaveButton(saveName, 1));
     }
 
     private void startSaveButtonAction(Event evt) {
@@ -46,6 +52,14 @@ public class LauncherController {
         startSaveButton.setId(String.valueOf(saveId));
         startSaveButton.setOnAction(evt -> startSaveButtonAction(evt));
         return startSaveButton;
+    }
+
+    private String validateSaveName(String saveName){
+        Pattern saveNamePattern = Pattern.compile("^(?=.*[A-Za-z])(\\w| |-)*$");
+        if (!saveNamePattern.matcher(saveName).find()) {
+            throw new IllegalArgumentException("Save name must contain at loast one letter a-z, space, underscore and dash");
+        }
+        return saveName;
     }
 
 }
