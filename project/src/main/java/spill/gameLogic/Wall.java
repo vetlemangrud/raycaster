59f882df729f0ecd61ea7ec7591ef507fbbd482a
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-02-11 11:16:38 +0100

    Class for wall types

---
+++ project/src/main/java/spill/gameLogic/Wall.java
@@ -0,0 +1,15 @@
+package spill.gamelogic;
+
+import javafx.scene.paint.Color;
+
+public class Wall {
+    String type;
+    Color color; //Invisible if null
+    public Wall(String type, Color color) {
+        this.type = type;
+        this.color = color;
+    }
+
+}
+
+    
