fe868ca0ae72518e8fb6e7b4a3d85d331a6321da
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-02-12 09:31:41 +0100

    Created vector class

---
+++ project/src/main/java/spill/game/Vector.java
@@ -0,0 +1,12 @@
+package spill.game;
+
+import javafx.geometry.Point2D;
+
+public class Vector extends Point2D{
+
+    public Vector(double x, double y) {
+        super(x, y);
+        
+    }
+    
+}
