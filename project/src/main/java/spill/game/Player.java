d3e16e31584210a8aa92a7e153fd14497dc521c4
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-02-17 12:23:48 +0100

    Simple player class

--- project/src/main/java/spill/game/Player.java
+++ project/src/main/java/spill/game/Player.java
@@ -1,7 +1,31 @@
 package spill.game;
 
+import spill.game.util.Vector;
+
 public class Player {
-    double xPos;
-    double yPos;
+    Vector pos;
     double angle;
+
+    public Player(double x, double y){
+        pos = new Vector(x, y);
+        angle = 0;
+    }
+
+
+    public Vector getPos() {
+        return this.pos;
+    }
+
+    public void setPos(Vector pos) {
+        this.pos = pos;
+    }
+
+    public double getAngle() {
+        return this.angle;
+    }
+
+    public void setAngle(double angle) {
+        this.angle = angle;
+    }
+    
 }
