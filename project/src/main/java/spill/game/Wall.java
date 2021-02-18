687e11c691e90459f433211db536eb7ff5099dfe
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-02-18 22:57:00 +0100

    Moved static walls to walls class

--- project/src/main/java/spill/game/Wall.java
+++ project/src/main/java/spill/game/Wall.java
@@ -3,6 +3,10 @@ package spill.game;
 import javafx.scene.paint.Color;
 
 public class Wall {
+
+    public static final Wall Air = new Wall(null);
+    public static final Wall Green = new Wall(Color.GREEN);
+
     Color color; //Invisible if null
     public Wall(Color color) {
         this.color = color;
