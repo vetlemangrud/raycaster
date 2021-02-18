c263add71dea762960831797bfa6d2c504d028f0
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-02-18 19:31:17 +0100

    Removed unused class import

--- project/src/test/java/spill/StorageTest.java
+++ project/src/test/java/spill/StorageTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.IOException;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import spill.storage.Storage;
 
