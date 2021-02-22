7cb9f4cf7bfc8b60175bdba6711351bb9bba04cf
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-02-22 23:04:32 +0100

    Weird ass brightness based on distance

--- project/src/main/java/spill/rendering/RaycastRenderer.java
+++ project/src/main/java/spill/rendering/RaycastRenderer.java
@@ -1,9 +1,11 @@
 package spill.rendering;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.effect.Effect;
 import javafx.scene.paint.Color;
 import spill.game.Game;
 import spill.game.util.RayCaster;
+import spill.game.util.RayHit;
 import spill.game.util.Vector;
 
 public class RaycastRenderer extends Renderer {
@@ -17,12 +19,13 @@ public class RaycastRenderer extends Renderer {
     @Override
     public void render() {
         gc.clearRect(0,0,canvasWidth,canvasHeight);
-        gc.setFill(Color.GREEN);
+       
         for (int i = 0; i < RAY_COUNT; i++) {
-            Vector hit = RayCaster.hitWall(game.getPlayer().getPos(), game.getPlayer().getDirection().copy().rotate(FOV * i/RAY_COUNT - FOV/2), game.getCurrentLevel()).getPosition();
-            double rayDistance = Vector.distance(game.getPlayer().getPos(), hit);
-            double fisheyeCorrectedDistance = rayDistance * Math.cos(game.getPlayer().getDirection().getAngle() - hit.sub(game.getPlayer().getDirection()).getAngle());
-            double lineHeight = canvasHeight/rayDistance;
+            RayHit hit = RayCaster.hitWall(game.getPlayer().getPos(), game.getPlayer().getDirection().copy().rotate(FOV * i/RAY_COUNT - FOV/2), game.getCurrentLevel());
+            double rayDistance = Vector.distance(game.getPlayer().getPos(), hit.getPosition());
+            double fisheyeCorrectedDistance = rayDistance * Math.cos(game.getPlayer().getDirection().getAngle() - hit.getAngle());
+            double lineHeight = canvasHeight/fisheyeCorrectedDistance;
+            gc.setFill(hit.getWall().getColor().deriveColor(0, 1, 2* hit.getAngle()/Math.PI, 1));
             gc.fillRect(canvasWidth * i/RAY_COUNT, canvasHeight/2 - lineHeight/2, canvasWidth/RAY_COUNT, lineHeight);
         }
     }
