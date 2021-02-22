f83ed6bc7ec4503b7a0365448fc92fbd1c440034
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-02-22 21:01:47 +0100

    Raycaster works!!! But it smells badly

--- project/src/main/java/spill/game/util/RayCaster.java
+++ project/src/main/java/spill/game/util/RayCaster.java
@@ -2,18 +2,19 @@ package spill.game.util;
 
 import spill.game.Level;
 public class RayCaster {
-    private static final int DEPTH_OF_FIELD = 8;
+    //TODO: THIS CODE SMELLS BADLY, BUT WORKS, MAKE IT EASIER TO UNDERSTAND!!!
+    private static final int DEPTH_OF_FIELD = 8; //How many blocks the ray can travel max 
     //Thanks to Lode for much inspiration and help https://lodev.org/cgtutor/raycasting.html
     //Also thanks to 3DSage https://www.youtube.com/watch?v=gYRrGTC7GtA&t=467s
     public static Vector hitWall(Vector origin, Vector direction, Level level){
-        Vector closestHorizontal; //First point it can hit
-        Vector deltaHorizontal; //Vector between horizontal lines
+        
+        Vector horizontalHit; //Point where ray hits a wall (Just checking horizontal walls) (Global position)
+        Vector verticalHit; //Point where ray hits a wall (Just checking vertical walls) (Global position)
 
         //Check horizontal faces
         if (direction.getAngle() == 0 || direction.getAngle() == Math.PI) {
             //Wont see any horizontal face if looking perpendicular to the y axis
-            deltaHorizontal = new Vector(0,0);
-            closestHorizontal = new Vector(0,0);
+            horizontalHit = new Vector(0,Double.MAX_VALUE);
         } else {
             double closestY;
             double deltaY;
@@ -26,10 +27,53 @@ public class RayCaster {
                 closestY = Math.floor(origin.getY()); 
                 deltaY = -1;
             }
-            deltaHorizontal = new Vector(deltaY * (1/Math.tan(direction.getAngle())), deltaY);
-            closestHorizontal = new Vector(closestY * (1/Math.tan(direction.getAngle())) + origin.getX(), closestY);
-        }
-        return closestHorizontal;
 
+            Vector nextPoint = Vector.mult(direction, (closestY-origin.getY())/direction.getY()).add(origin); //First point it can hit (Global position)
+            Vector deltaHorizontal = Vector.mult(direction, deltaY/direction.getY()); //Vector between horizontal lines (Relative position)
+
+            double linesCrossed = 0;
+            while (linesCrossed < DEPTH_OF_FIELD) {
+                if (level.getWall(Vector.add(nextPoint,new Vector(0,-0.1))).getColor() == null && level.getWall(Vector.add(nextPoint,new Vector(0,0.1))).getColor() == null) {
+                    nextPoint.add(deltaHorizontal);
+                }
+                linesCrossed++;
+            }
+            horizontalHit = nextPoint;
+        }
+
+        //Check vertical faces
+        if (direction.getAngle() == Math.PI/4 || direction.getAngle() == Math.PI*6/4) {
+            //Wont see any vertical face if looking perpendicular to the x axis
+            verticalHit = new Vector(Double.MAX_VALUE,0);
+        } else {
+            double closestX;
+            double deltaX;
+            if(direction.getAngle() < Math.PI/2 || direction.getAngle() > Math.PI * 3/2){
+                //Looking rigth
+                closestX = Math.ceil(origin.getX()); 
+                deltaX = 1;
+            } else {
+                //Looking left
+                closestX = Math.floor(origin.getX()); 
+                deltaX = -1;
+            }
+
+            Vector nextPoint = Vector.mult(direction, (closestX-origin.getX())/direction.getX()).add(origin); //First point it can hit (Global position)
+            Vector deltaVertical = Vector.mult(direction, deltaX/direction.getX()); //Vector between vertical lines (Relative position)
+
+            double linesCrossed = 0;
+            while (linesCrossed < DEPTH_OF_FIELD) {
+                if (level.getWall(Vector.add(nextPoint,new Vector(-0.1,0))).getColor() == null && level.getWall(Vector.add(nextPoint,new Vector(0.1,0))).getColor() == null) {
+                    nextPoint.add(deltaVertical);
+                }
+                linesCrossed++;
+            }
+            verticalHit = nextPoint;
+        }
+        if (Vector.distance(origin, verticalHit) < Vector.distance(origin, horizontalHit)) {
+            return verticalHit;
+        } else {
+            return horizontalHit;
+        }
     }
 }
