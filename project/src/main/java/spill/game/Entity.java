a27a6ffd5bfb27e4dbeec5bbfb3c022bd8c22e97
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-03-30 21:32:41 +0200

    Made class for entities

---
+++ project/src/main/java/spill/game/Entity.java
@@ -0,0 +1,20 @@
+package spill.game;
+
+import javafx.scene.image.Image;
+import spill.game.util.Vector;
+
+public class Entity{
+    private Vector pos;
+    private Image sprite;
+    private Level level;
+
+    public Entity(Level level, Vector startPosition, String spriteName){
+        sprite = new Image(getClass().getResourceAsStream("/sprite/" + spriteName + ".png"));
+        this.level = level;
+        this.pos = startPosition;
+    }
+
+    public Vector getPos(){
+        return pos;
+    }
+}
